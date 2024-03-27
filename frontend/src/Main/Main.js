ab66e968a730fcffca0684f047c3c8d396a2c4ef
“Sergio <sergncp@amazon.com>
2024-03-27 10:40:43 -0700

    fixed tiered cost function. Ties VPC Endpoint number to deployed AZ number close #13 close #14

--- frontend/src/Main/Main.js
+++ frontend/src/Main/Main.js
@@ -803,7 +803,7 @@ export default class Main extends React.Component {
 
                                                         {/* Account A Bottom Row*/}
                                                         <ArcherElement id="vpcaa"
-                                                            relations={((this.state.dx || this.state.vpn || this.state.interRegion || this.state.r53res_inbound || this.state.r53res_outbound || this.state.tgw ) ^ this.state.nwfw_c)? [ 
+                                                            relations={((this.state.dx || this.state.vpn || this.state.interRegion || this.state.r53res_inbound || this.state.r53res_outbound || this.state.tgw ) ^ (this.state.nwfw_c || this.state.vpce_c))? [ 
                                                                 {
                                                                 targetId:'t4-tgw',
                                                                 targetAnchor: 'top',
